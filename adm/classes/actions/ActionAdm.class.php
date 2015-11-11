2abf9e7634bb77dd47d512c0cf5a78bdda47b72b
tema_mak <temamakdeveloper@gmail.com>
2015-11-11 19:05:54 +0400

    fix profile page for non auth user

--- adm/classes/actions/ActionAdm.class.php
+++ adm/classes/actions/ActionAdm.class.php
@@ -7,9 +7,6 @@ class PluginAdm_ActionAdm extends ActionPlugin {
 
 
 	public function Init() {		
-		if (!$this->User_IsAuthorization()) {
-			return parent::EventNotFound();
-		}
 		$this->oUserCurrent=$this->User_GetUserCurrent();
 		$this->SetDefaultEvent('profile');
 	}
@@ -21,61 +18,67 @@ class PluginAdm_ActionAdm extends ActionPlugin {
 	}
 
 	protected function EventProfile() {
-		if (!($this->oUserCurrent)){
-			return parent::EventNotFound();
-		}			
-		
-		$iYear = $this->PluginAdm_Adm_GetYear();
-		$bIsEnable = $this->PluginAdm_Adm_isEnable();
-		
-		if (isPost('submit_adm_profile')) {
-			$oNewAdmProfile = Engine::GetEntity('PluginAdm_Admprofile');			
-			$oNewAdmProfile->setAddress(getRequest('adm_profile_addr'));
-			$oNewAdmProfile->setName(getRequest('adm_profile_name'));
+		if (!($this->oUserCurrent)){		
+			$this->Viewer_Assign('bNoUserCurrent',true);
+		}	else {
+			$iYear = $this->PluginAdm_Adm_GetYear();
+			$bIsEnable = $this->PluginAdm_Adm_isEnable();
 			
-			$this->PluginAdm_Admprofile_SetAdmProfile($this->oUserCurrent,$oNewAdmProfile,$iYear);
-		}
-		
-		if (isPost('submit_i_am_send_gift')) {
-			$this->PluginAdm_Admprofile_setGiftSend($this->oUserCurrent,$iYear);
-		}
-		
-		if (isPost('submit_i_am_recive_gift')) {
-			$this->PluginAdm_Admprofile_setGiftRecive($this->oUserCurrent,$iYear);
+			if (isPost('submit_adm_profile')) {
+				$oNewAdmProfile = Engine::GetEntity('PluginAdm_Admprofile');
+				$oNewAdmProfile->setAddress(getRequest('adm_profile_addr'));
+				$oNewAdmProfile->setName(getRequest('adm_profile_name'));
+					
+				$this->PluginAdm_Admprofile_SetAdmProfile($this->oUserCurrent,$oNewAdmProfile,$iYear);
+			}
+			
+			if (isPost('submit_i_am_send_gift')) {
+				$this->PluginAdm_Admprofile_setGiftSend($this->oUserCurrent,$iYear);
+			}
+			
+			if (isPost('submit_i_am_recive_gift')) {
+				$this->PluginAdm_Admprofile_setGiftRecive($this->oUserCurrent,$iYear);
+			}
+			
+			$oAdmProfile = $this->PluginAdm_Admprofile_GetAdmProfile($this->oUserCurrent,$iYear);
+			$oAdmOldProfile = $this->PluginAdm_Admprofile_GetOldAdmProfiles($this->oUserCurrent,$iYear);
+			
+			if ($oAdmProfile){
+				$this->Viewer_Assign('sAdmName',$oAdmProfile->getName());
+				$this->Viewer_Assign('sAdmAddress',$oAdmProfile->getAddress());
+					
+				if($oAdmProfile->getReciverUserId()){
+					$oReciverUser = $this->User_GetUserById($oAdmProfile->getReciverUserId());
+					$this->Viewer_Assign('oAdmReciverProfile',$this->PluginAdm_Admprofile_GetAdmProfile($oReciverUser,$iYear));
+				}
+			} else {
+				$this->Viewer_Assign('sAdmName','');
+				$this->Viewer_Assign('sAdmAddress','');
+			}
+			
+			/*
+			 * Пробуем получить данные из прошлых профилей АДМ
+			*/
+			if($oAdmOldProfile && !$oAdmProfile){
+				$oLastAdmProfile = end($oAdmOldProfile);
+					
+				$this->Viewer_Assign('sAdmName',$oLastAdmProfile->getName());
+				$this->Viewer_Assign('sAdmAddress',$oLastAdmProfile->getAddress());
+			}
+			
+			$this->Viewer_Assign('oAdmProfile',$oAdmProfile);
+			$this->Viewer_Assign('iYear',$iYear);
+			$this->Viewer_Assign('bIsEnable',$bIsEnable);			
 		}		
 		
-		$oAdmProfile = $this->PluginAdm_Admprofile_GetAdmProfile($this->oUserCurrent,$iYear);		
-		$oAdmOldProfile = $this->PluginAdm_Admprofile_GetOldAdmProfiles($this->oUserCurrent,$iYear);
-		
-		if ($oAdmProfile){
-			$this->Viewer_Assign('sAdmName',$oAdmProfile->getName());
-			$this->Viewer_Assign('sAdmAddress',$oAdmProfile->getAddress());
-			
-			if($oAdmProfile->getReciverUserId()){
-				$oReciverUser = $this->User_GetUserById($oAdmProfile->getReciverUserId());
-				$this->Viewer_Assign('oAdmReciverProfile',$this->PluginAdm_Admprofile_GetAdmProfile($oReciverUser,$iYear));				
-			}
-		} else {
-			$this->Viewer_Assign('sAdmName','');
-			$this->Viewer_Assign('sAdmAddress','');			
-		}
-
-		/*
-		 * Пробуем получить данные из прошлых профилей АДМ
-		 */
-		if($oAdmOldProfile && !$oAdmProfile){
-			$oLastAdmProfile = end($oAdmOldProfile);
-			
-			$this->Viewer_Assign('sAdmName',$oLastAdmProfile->getName());
-			$this->Viewer_Assign('sAdmAddress',$oLastAdmProfile->getAddress());
-		}
-		
-		$this->Viewer_Assign('oAdmProfile',$oAdmProfile);
-		$this->Viewer_Assign('iYear',$iYear);
-		$this->Viewer_Assign('bIsEnable',$bIsEnable);		
+	
 	}
 
 	protected function EventAdmin(){
+		if (!$this->User_IsAuthorization()) {
+			return parent::EventNotFound();
+		}		
+		
     	if (!$this->User_GetUserCurrent()->isAdministrator()){
     		return parent::EventNotFound();
 		}			
@@ -86,6 +89,10 @@ class PluginAdm_ActionAdm extends ActionPlugin {
 	}
 	
 	protected function EventMakeAdm(){
+		if (!$this->User_IsAuthorization()) {
+			return parent::EventNotFound();
+		}		
+		
     	if (!$this->User_GetUserCurrent()->isAdministrator()){
     		return parent::EventNotFound();
 		}			
--- adm/templates/skin/default/actions/ActionAdm/profile.tpl
+++ adm/templates/skin/default/actions/ActionAdm/profile.tpl
@@ -1,48 +1,52 @@
 {assign var="sidebarPosition" value='left'}
 {include file='header.tpl'}
 <h2 class="page-header">{$aLang.plugin.adm.profile}</h2>
+{if $bNoUserCurrent}
+    {$aLang.plugin.adm.no_auth} <a href="/login">{$aLang.plugin.adm.no_auth_link}</a>
+{else}
 
-{if !$oAdmProfile}
-	<div class="adm_profile_info">
-		{$aLang.plugin.adm.not_adm_profile}
-	</div>
+    {if !$oAdmProfile}
+        <div class="adm_profile_info">
+            {$aLang.plugin.adm.not_adm_profile}
+        </div>
+    {/if}
+
+    {if $oAdmReciverProfile}
+        <span class="adm_profile_info">
+            {$aLang.plugin.adm.reciver_user}:<br />
+            {$oAdmReciverProfile->getName()}<br />
+            {$oAdmReciverProfile->getAddress()}
+        </span>
+        
+        <form method="post" enctype="multipart/form-data" class="form-profile" id="adm_form_gift_state">
+            <button type="submit"  name="submit_i_am_send_gift" class="button button-primary adm_button" {if $oAdmProfile->GetIsGiftSend() == 1} disabled="disabled"{/if}/>{$aLang.plugin.adm.i_am_send_gift}</button>
+            <button type="submit"  name="submit_i_am_recive_gift" class="button button-primary adm_button" {if $oAdmProfile->GetIsGiftRecive() == 1} disabled="disabled"{/if}/>{$aLang.plugin.adm.i_am_recive_gift}</button>
+        </form>
+    {/if} 
+
+    <form method="post" enctype="multipart/form-data" class="form-profile" id="adm_form">
+        <table style="width:100%">
+            <tr class="adm_profile">
+                <td width="150px">
+                {$aLang.plugin.adm.reciver_name}:
+                </td>
+                <td >
+                    <input type="text" name="adm_profile_name" id="adm_profile_name" class="input-text input-width-full" value="{$sAdmName}" />
+                </td>
+            </tr>   
+            <tr class="adm_profile">
+                <td width="150px">
+                {$aLang.plugin.adm.recive_addr}:
+                </td>
+                <td >
+                    <input type="text" name="adm_profile_addr" id="adm_profile_addr" class="input-text input-width-full" value="{$sAdmAddress}" />
+                </td>
+            </tr>                   
+        </table>
+        
+        <button type="submit"  name="submit_adm_profile" class="button button-primary" {if $oAdmReciverProfile} disabled="disabled" {/if}/>{if !$oAdmProfile}{$aLang.plugin.adm.create_profile}{else}{$aLang.plugin.adm.update_profile}{/if}</button>
+        
+    </form>
 {/if}
-
-{if $oAdmReciverProfile}
-	<span class="adm_profile_info">
-		{$aLang.plugin.adm.reciver_user}:<br />
-		{$oAdmReciverProfile->getName()}<br />
-		{$oAdmReciverProfile->getAddress()}
-	</span>
-	
-	<form method="post" enctype="multipart/form-data" class="form-profile" id="adm_form_gift_state">
-		<button type="submit"  name="submit_i_am_send_gift" class="button button-primary adm_button" {if $oAdmProfile->GetIsGiftSend() == 1} disabled="disabled"{/if}/>{$aLang.plugin.adm.i_am_send_gift}</button>
-		<button type="submit"  name="submit_i_am_recive_gift" class="button button-primary adm_button" {if $oAdmProfile->GetIsGiftRecive() == 1} disabled="disabled"{/if}/>{$aLang.plugin.adm.i_am_recive_gift}</button>
-	</form>
-{/if} 
-
-<form method="post" enctype="multipart/form-data" class="form-profile" id="adm_form">
-	<table style="width:100%">
-		<tr class="adm_profile">
-			<td width="150px">
-			{$aLang.plugin.adm.reciver_name}:
-			</td>
-			<td >
-				<input type="text" name="adm_profile_name" id="adm_profile_name" class="input-text input-width-full" value="{$sAdmName}" />
-			</td>
-		</tr>	
-		<tr class="adm_profile">
-			<td width="150px">
-			{$aLang.plugin.adm.recive_addr}:
-			</td>
-			<td >
-				<input type="text" name="adm_profile_addr" id="adm_profile_addr" class="input-text input-width-full" value="{$sAdmAddress}" />
-			</td>
-		</tr>					
-	</table>
-	
-	<button type="submit"  name="submit_adm_profile" class="button button-primary" {if $oAdmReciverProfile} disabled="disabled" {/if}/>{if !$oAdmProfile}{$aLang.plugin.adm.create_profile}{else}{$aLang.plugin.adm.update_profile}{/if}</button>
-	
-</form>
 
 {include file='footer.tpl'}
